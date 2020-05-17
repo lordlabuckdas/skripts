24e2169e22945ad927f15c9767673c09330f3a10
krishna <55460753+lordlabuckdas@users.noreply.github.com>
2020-05-17 14:26:51 +0530

    made some comments and changes

--- miniurl/miniurl.py
+++ miniurl/miniurl.py
@@ -3,7 +3,10 @@ from selenium.webdriver.common.keys import Keys
 import pyperclip
 from time import sleep
 
-driver=webdriver.Firefox(executable_path="./geckodriver")
+#if geckodriver isn't in PATH, copy it to the folder of this file,
+#uncomment next line and delete line 9
+#driver=webdriver.Firefox(executable_path="./geckodriver") 
+driver=webdriver.Firefox()
 driver.get("https://bitly.com/")
 sleep(1)
 urll=driver.find_element_by_xpath('//*[@id="shorten_url"]')

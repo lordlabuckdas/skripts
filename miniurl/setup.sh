a9d74695aeb1c085a4f295015700d8f94d62f072
krishna <55460753+lordlabuckdas@users.noreply.github.com>
2020-05-18 20:33:12 +0530

    updated readme

--- miniurl/setup.sh
+++ miniurl/setup.sh
@@ -3,8 +3,10 @@ wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodrive
 tar -xvzf geckodriver-v0.26.0-linux64.tar.gz
 rm geckodriver-v0.26.0-linux64.tar.gz
 cp geckodriver /usr/local/bin
-chmod +x /usr/local/bin/geckodriver
-cp miniurl.py /usr/local/bin/miniurl
-chmod +x /usr/local/bin/miniurl
+chmod 755 /usr/local/bin/geckodriver
+cp miniurl.py $HOME
+chmod +x $HOME/miniurl.py
+echo "#!/usr/bin/bash" >> /usr/local/bin/miniurl
+echo "python3 $HOME/miniurl.py" >> /usr/local/bin/miniurl
 python3 -m pip install -r requirements.txt
 echo "installation complete!"

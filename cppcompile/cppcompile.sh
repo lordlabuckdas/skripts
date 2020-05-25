0104e1c0587d16571603f32024715b6202207431
krishna <55460753+lordlabuckdas@users.noreply.github.com>
2020-05-25 11:24:33 +0530

    made sure the code is executed only if it's compiled properly using &&

--- cppcompile/cppcompile.sh
+++ cppcompile/cppcompile.sh
@@ -4,5 +4,4 @@ fcode=${1-69}
 cp template.cpp code${fcode}.cpp
 nvim code${fcode}.cpp
 nvim input.txt
-g++ code${fcode}.cpp -o code${fcode}.out
-./code${fcode}.out < input.txt
+g++ code${fcode}.cpp -o code${fcode}.out && ./code${fcode}.out < input.txt

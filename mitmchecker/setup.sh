23cfd5a44a5275281d2584444f2c8bf769ea8444
krishna <55460753+lordlabuckdas@users.noreply.github.com>
2020-11-03 12:12:12 +0530

    updated script

--- mitmchecker/setup.sh
+++ mitmchecker/setup.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
+# copies file to home directory for persistence
 cp mitm_checker.sh $HOME/
+# get arp tables
 arp -e | grep : | cut -d ' ' -f 1,21 > /tmp/init_arptab.txt
 echo "added checker script to home directory"
+# make it executable
 chmod +x $HOME/mitm_checker.sh
+# set up cronjob to check continuously
 crontab cronjob.txt
 echo "added cronjob"
 echo "done setting up!"
